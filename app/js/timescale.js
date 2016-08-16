14577729980d1353baf06216f274cba77f61f39d
Edwin Chen <mredwinchenv@gmail.com>
2016-08-16 23:24:11 +0800

    update

--- app/js/timescale.js
+++ app/js/timescale.js
@@ -6,6 +6,7 @@ var timescaleWidth = 15 + (timescaleNum *9.16);
 $("#timescale").css({"width":timescaleWidth});
 for(var l= 0 ; l < timescaleNum ; l++){
     var i = l+1;
+
     $("#timescale").append("<li id='"+ i +"'></li>");
     if(i % 5 == 0){
         $("#"+ i +"").css({"height":"35px"});
@@ -16,9 +17,21 @@ for(var l= 0 ; l < timescaleNum ; l++){
         var n = i*100;
         $("#"+ i +"").append("<span>"+ n +"</span>");
     }
-
+    var s = $("#"+ i +"").position().left;
+    $("#"+ i +"").attr("data-num",s);
 }
 var x= 0;
 $(".timescale").scroll(function () {
-    $("#timescaleValue").html(x+=1);
-})
+    var y = x+=100;
+    for(var m= 0 ; m < timescaleNum ; m++){
+        var k = m+1;
+        var g = k*100;
+        var o = $("#"+ k +"").position().left;
+        if(y == g){
+            $(this).scrollLeft(o);
+            console.log(y);
+        };
+    }
+
+    $("#timescaleValue").html(y);
+});
