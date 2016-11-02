330361844f66b21658df84297bf014eb1c70048a
Edwin Chen <mredwinchenv@gmail.com>
2016-11-02 15:26:30 +0800

    update
    update

--- app/js/timescale.js
+++ app/js/timescale.js
@@ -1,37 +1,34 @@
-/**
- * Created by fcm on 16/8/12.
- */
-var timescaleNum = 190;
-var timescaleWidth = 15 + (timescaleNum *9.16);
-$("#timescale").css({"width":timescaleWidth});
-for(var l= 0 ; l < timescaleNum ; l++){
-    var i = l+1;
-
-    $("#timescale").append("<li id='"+ i +"'></li>");
-    if(i % 5 == 0){
-        $("#"+ i +"").css({"height":"35px"});
-        var n = i*100;
-        $("#"+ i +"").append("<span>"+ n +"</span>");
-    }else if (i == 1){
-        $("#"+ i +"").css({"height":"35px"});
-        var n = i*100;
-        $("#"+ i +"").append("<span>"+ n +"</span>");
+function startTimecale(number, time) {
+    var num = number;
+    for (l = time; l < num; l++) {
+        var i = l + 1;
+        $(".swiper-wrapper").append('<div class="swiper-slide" id="' + i + '"></div>')
+        if (i % 5 == 0) {
+            $("#" + i + "").addClass("ssh");
+            var n = i * 100;
+            $("#" + i + "").append("<span>" + n + "</span>");
+        } else if (i == 1) {
+            $("#" + i + "").addClass("ssh");
+            var n = i * 100;
+            $("#" + i + "").append("<span>" + n + "</span>");
+        }
     }
-    var s = $("#"+ i +"").position().left;
-    $("#"+ i +"").attr("data-num",s);
 }
-var x= 0;
-$(".timescale").scroll(function () {
-    var y = x+=100;
-    for(var m= 0 ; m < timescaleNum ; m++){
-        var k = m+1;
-        var g = k*100;
-        var o = $("#"+ k +"").position().left;
-        if(y == g){
-            $(this).scrollLeft(o);
-            console.log(y);
-        };
+startTimecale(150, 0);
+$(".numtitle").html(100);
+var swiper = new Swiper('.swiper-container', {
+    pagination: '.swiper-pagination',
+    slidesPerView: 20,
+    speed: 50,
+    centeredSlides: true,
+    paginationClickable: true,
+    spaceBetween: 0,
+    paginationType: 'fraction',
+    paginationFractionRender: function (swiper, currentClassName) {
+        return '<span class="' + currentClassName + '" id="num"></span>';
+    },
+    onSlideChangeEnd: function (swiper) {
+        var numTitle = (swiper.activeIndex + 1) * 100;
+        $(".numtitle").html(numTitle)
     }
-
-    $("#timescaleValue").html(y);
 });
